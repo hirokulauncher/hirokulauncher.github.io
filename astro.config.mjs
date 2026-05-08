f2b90bcdfa7651a06fc252848f964864d0480c07
chann1337 <brussia475@gmail.com>
2026-05-08 03:19:45 +0300

    feat: migrate to hirokulauncher.github.io org

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://chann1337.github.io',
-  base: '/Hiroku-Launcher-Web',
+  site: 'https://hirokulauncher.github.io',
 });
